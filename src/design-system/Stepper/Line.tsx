a32be4a83eb08b016cf2c9e33a91006e70e7f41b
Elo89 <elia.morini.1989@gmail.com>
2022-07-26 23:29:17 +0200

    feature(Stepper): add Stepper components

---
+++ src/design-system/Stepper/Line.tsx
@@ -0,0 +1,23 @@
+import { Div } from '../Styled';
+
+import 'swiper/css';
+interface PropType {
+  indexActive: number,
+  index: number,
+}
+
+function Line({ indexActive, index }: PropType) {
+  return (
+    <Div
+      borderTop={["6px solid", "0 solid"]}
+      borderLeft={["0 solid", "6px solid"]}
+      borderRadius={'3px'}
+      borderColor={indexActive >= index ? ["primary", "primary"] : ["white", "white"]}
+      width={['30px', 0 ]}
+      height={[0, '50px']}
+      m={2}
+    />
+  );
+}
+
+export default Line;
---
+++ src/design-system/Stepper/index.tsx
@@ -0,0 +1,50 @@
+import { Flex } from '../Styled';
+import { Text1 } from '../Typography';
+import 'swiper/css';
+import Line from './Line';
+import { useCallback, useMemo } from 'react';
+
+interface PropType {
+  surveyConf: any,
+  indexActive: number,
+}
+
+function Stepper({ surveyConf, indexActive }: PropType) {
+  const stepColor = useCallback((index: number) => {
+    if (indexActive === index) 
+      return 'highlight'; 
+    if (indexActive >= index) 
+      return 'primary'; 
+    return 'white'
+  }, [indexActive]);
+
+  return (
+    <Flex
+      flexDirection={['row', 'column']}
+      alignItems="center"
+    >
+      {surveyConf?.map((survey: any, index: number) =>
+        <Flex 
+          key={`step-${index}`}
+          alignItems={["flex-start", "center"]}
+          flexDirection={['row', 'column']}
+          flex={1}
+        >
+          {index !== 0 && 
+            <Line indexActive={indexActive} index={index} />
+          }
+          <Text1 
+            width={'55px'}
+            color={stepColor(index)}
+          >
+            {survey.stepName}
+          </Text1>
+        </Flex>
+      )}
+    </Flex>
+  );
+}
+
+
+
+export default Stepper;
