bbbbf01edb078dd199a7f73c956e1afc9aa7d306
Elo89 <elia.morini.1989@gmail.com>
2022-07-26 23:30:08 +0200

    feature(Theme): add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { css, Global, ThemeProvider } from '@emotion/react'
 import { theme } from './theme/theme';
 import Survey from './Survey';
 import surveyConf from './surveyConf';
 import emotionReset from 'emotion-reset';
+import Header from './design-system/Header';
+import { Flex } from './design-system/Styled';
 
 function App() {
   return (
@@ -11,7 +12,18 @@ function App() {
       <Global 
         styles={css`
           ${emotionReset}
+          body {
+            font-family: 'Fuzzy Bubbles', cursive;
+            font-size: 10px;
+          }
 
+          .swiper.swiper-initialized {
+            width: 100%;
+            height: calc(100vh - 150px);
+            @media screen and (max-width: 640px) {
+              height: calc(100vh - 230px);
+            }
+          }
           *, *::after, *::before {
             box-sizing: border-box;
             -moz-osx-font-smoothing: grayscale;
@@ -20,7 +32,10 @@ function App() {
           }
         `} 
       />
-      <Survey surveyConf={surveyConf} />
+      <Flex flexDirection="column">
+        <Header />
+        <Survey surveyConf={surveyConf} />
+      </Flex>
     </ThemeProvider>
   );
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -4,20 +4,104 @@ declare module '@emotion/react' {
   export interface Theme {
     colors: {
         primary: string;
-        background: string;
+        highlight: string;
         error: string;
         success: string;
-        disaled: string;
+        disabled: string;
     };
+    fontSizes: {
+      xs: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+    },
+    space: {
+      px: string;
+      0.5: string;
+      1: string;
+      1.5: string;
+      2: string;
+      2.5: string;
+      3: string;
+      3.5: string;
+      4: string;
+      5: string;
+      6: string;
+      7: string;
+      8: string;
+      9: string;
+      10: string;
+      12: string;
+      14: string;
+      16: string;
+      20: string;
+      24: string;
+      28: string;
+      32: string;
+      36: string;
+      40: string;
+      44: string;
+      48: string;
+      52: string;
+      56: string;
+      60: string;
+      64: string;
+      72: string;
+      80: string;
+      96: string;
+    },
   }
 }
 
 export const theme = {
-    colors: {
-        primary: '#fff',
-        background: '#000',
-        error: '#A85661',
-        success: '#76A653',
-        disaled: '#989898'
-    },
+  colors: {
+    primary: 'hotpink',
+    highlight: '#cc3680',
+    error: '#A85661',
+    success: '#76A653',
+    disabled: '#ccc'
+  },
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+  },
+  space: {
+    px: '1px',
+    0.5: '0.125rem',
+    1: '0.25rem',
+    1.5: '0.375rem',
+    2: '0.5rem',
+    2.5: '0.625rem',
+    3: '0.75rem',
+    3.5: '0.875rem',
+    4: '1rem',
+    5: '1.25rem',
+    6: '1.5rem',
+    7: '1.75rem',
+    8: '2rem',
+    9: '2.25rem',
+    10: '2.5rem',
+    12: '3rem',
+    14: '3.5rem',
+    16: '4rem',
+    20: '5rem',
+    24: '6rem',
+    28: '7rem',
+    32: '8rem',
+    36: '9rem',
+    40: '10rem',
+    44: '11rem',
+    48: '12rem',
+    52: '13rem',
+    56: '14rem',
+    60: '15rem',
+    64: '16rem',
+    72: '18rem',
+    80: '20rem',
+    96: '24rem',
+  },
 };
