25ae7233192bfadb94bbbdc45c2cae7d82fa4d0c
Elo89 <elia.morini.1989@gmail.com>
2022-07-26 23:25:05 +0200

    feature(Header): add Header component

---
+++ src/design-system/Header/index.tsx
@@ -0,0 +1,22 @@
+import { Flex } from '../Styled';
+import { Header1 } from '../Typography';
+
+const iconPath = process.env.PUBLIC_URL + '/assets/images/';
+
+function Header() {
+  return (
+    <Flex 
+      alignItems="center"
+      justifyContent="center"
+      bg="#00D7FF"
+      height="150px"
+    >
+      <img src={`${iconPath}logo.png`} width="100px" style={{
+        margin: "20px"
+      }}/>
+      <Header1 color="primary" fontWeight={700} mr={4}>Survey for Kids</Header1>
+    </Flex>
+  );
+}
+
+export default Header;
